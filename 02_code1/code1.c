7f74edaa72c08f8832f79b3f193c875aeb7a073f
Coursera Learner <nobody@nowhere.nul>
2020-11-08 16:26:19 +0000

    Did assignment 2

--- 02_code1/code1.c
+++ 02_code1/code1.c
@@ -1,7 +1,13 @@
 int max (int num1, int num2) {
   //check if num1 is greater than num2
+  if (num1 > num2){
     //if so, your answer is num1
-    //otherwise, your answer is num2
+    return num1;
+  }
+  //otherwise, your answer is num2
+  else{
+    return num2;
+  }
 }
 
 int main(void) {
