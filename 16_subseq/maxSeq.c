0f993830576728db2f19ee1f9d5245c2c647ba36
Coursera Learner <nobody@nowhere.nul>
2020-11-09 15:47:11 +0000

    Did assignment 16

---
+++ 16_subseq/maxSeq.c
@@ -0,0 +1,33 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int compareNum(int num1, int num2){
+  if (num1>=num2){
+    return 0;
+  }
+  if (num1<num2){
+    return 1;
+  }
+}
+
+size_t maxSeq(int * array, size_t n){
+  if (n==0){
+    return 0;
+  }
+
+  int currLen = 1;
+  int maxLen = 1;
+  for (int i = 0; i < n-1; i++){
+    if (compareNum(array[i],array[i+1])==1){
+      currLen++;
+      if (currLen > maxLen){
+	maxLen = currLen;
+      }
+    }
+    else{
+      currLen = 1;
+    }
+  }
+  return maxLen;
+}
