857c06e323a55e22a919db275983d21b99b9bd6b
Coursera Learner <nobody@nowhere.nul>
2020-11-10 13:39:03 +0000

    Did assignment 30

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -16,9 +16,54 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+void printData(char** linearr, int count){
+  for (int i = 0; i < count; i++){
+    printf("%s",linearr[i]);
+    free(linearr[i]);
+  }
+  free(linearr);
+}
+
+void readFile(FILE* f){
+  char* line = NULL;
+  char** linearr = NULL;
+  size_t sz = 0;
+  size_t linecount = 0;
+
+  while(getline(&line, &sz, f) >= 0){
+    linearr=realloc(linearr, (1+linecount)*sizeof(*linearr));
+    linearr[linecount] = line;
+    line = NULL;
+    linecount++;
+  }
+  free(line);
+
+  sortData(linearr,linecount);
+  printData(linearr,linecount);
+}
+
 int main(int argc, char ** argv) {
   
   //WRITE YOUR CODE HERE!
-  
+  if (argc < 2) {
+    fprintf(stderr,"Usage: at least one argument must be provided. eg sortLines *input1* *input2* *input3* ...\n");
+    return EXIT_FAILURE;
+  }
+
+  for (int i = 0; i < argc-1;i++){
+    FILE * f = fopen(argv[i+1], "r");
+    if (f == NULL) {
+      perror("Could not open file");
+      return EXIT_FAILURE;
+    }
+
+    readFile(f);
+
+    if (fclose(f) != 0) {
+      perror("Failed to close the input file!");
+      return EXIT_FAILURE;
+    }
+  }
+
   return EXIT_SUCCESS;
 }
